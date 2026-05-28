483afb1784454e9f8e3f549e7be7dc5e9b5fa7ee
Nikola Crhák <122156533+crhaxx@users.noreply.github.com>
2026-05-28 14:51:07 +0200

    Added classes

---
+++ Cichnamon.cs
@@ -0,0 +1,45 @@
+class Cichnamon
+{
+    public string Jmeno { get; set; }
+    public int Zdravi { get; set; }
+    public int MaxZdravi { get; set; }
+    public int BonusSilaUtoku { get; set; }
+    public Utok ZakladniUtok { get; set; }
+    public Utok SpecialniUtok { get; set; }
+
+
+    public Cichnamon(string jmeno, int zdravi, int maxZdravi, int bonusSilaUtoku, Utok zakladniUtok, Utok specialniUtok)
+    {
+        Jmeno = jmeno;
+        Zdravi = zdravi;
+        MaxZdravi = maxZdravi;
+        BonusSilaUtoku = bonusSilaUtoku;
+        ZakladniUtok = zakladniUtok;
+        SpecialniUtok = specialniUtok;
+    }
+
+    public void ZautocitZakladniUtok(Trener trener)
+    {
+
+    }
+
+    public void ZautocitSpecialniUtok(Trener trener) { }
+
+    public void SnizitZdravi(int hodnota)
+    {
+        Zdravi -= hodnota;
+        if (Zdravi < 0)
+        {
+            Zdravi = 0;
+        }
+    }
+
+    public void Uzdravit(int hodnota)
+    {
+        Zdravi += hodnota;
+        if (Zdravi > MaxZdravi)
+        {
+            Zdravi = MaxZdravi;
+        }
+    }
+}
